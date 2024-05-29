ecfd226c8f9e5fbca7ec1ae19e59e2d35257d153
silviachaluisa <veronica-14-1998@hotmail.com>
2024-05-28 23:31:54 -0500

    css

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Servir archivos estáticos desde el directorio "public"
+app.use(express.static('public'));
+
 // Lista de integrantes
 const integrantes = [
   { id: 1, nombre: "Silvia", apellido: "Chaluisa" },
@@ -30,21 +33,66 @@ app.get('/integrantes/:id', (req, res) => {
   }
 });
 
-// Ruta para presentar un HTML con algunos productos quemados
+
+
+  
+// Lista de productos con sus nombres e imágenes
+const productos = [
+  { nombre: "Lácteos", imagen: "/imagenes/img2.jpg" },
+  { nombre: "Embutidos", imagen: "/imagenes/img3.jpg" },
+  { nombre: "Frutas", imagen: "/imagenes/img4.jpg" }
+];
+
+// Generar opciones del menú desplegable
+const options = productos.map(producto => `<option value="${producto.nombre}">${producto.nombre}</option>`).join('');
+
+// Ruta para presentar un HTML con el menú desplegable
 app.get('/products', (req, res) => {
-  const html = `
+  const dropdown = `
     <html>
+      <head>
+        <link rel="stylesheet" type="text/css" href="/style.css">
+      </head>
       <body>
-        <h1>Productos</h1>
-        <ul>
-          <li>Lacteos</li>
-          <li>Embutidos</li>
-          <li>Frutas</li>
-        </ul>
+        <h1>Selecciona un producto:</h1>
+        <form action="/producto" method="GET">
+          <select name="producto">
+            ${options}
+          </select>
+          <button type="submit">Ver Producto</button>
+        </form>
       </body>
     </html>
   `;
-  res.send(html);
+  res.send(dropdown);
+});
+
+// Ruta para mostrar el producto seleccionado
+app.get('/producto', (req, res) => {
+  const selectedProduct = req.query.producto;
+  const producto = productos.find(p => p.nombre === selectedProduct);
+  
+  if (producto) {
+    const html = `
+      <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="/style.css">
+        </head>
+        <body>
+          <div class="container">
+            <h1>${producto.nombre}</h1>
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <form action="/products" method="GET">
+              <button type="submit">Volver al menú</button>
+            </form>
+          </div>
+        </body>
+      </html>
+    `;
+    res.send(html);
+  } else {
+    res.send("Producto no encontrado");
+  }
 });
 
 app.listen(port, () => {
